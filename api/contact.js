c5df36f49558f6b61d073d96025954a581e04317
Dario Regazzoni <dario.regazzoni@outlook.fr>
2021-03-07 16:28:14 +0000

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -26,7 +26,7 @@ const transporter = nodemailer.createTransport({
 const sendMail = (req, res) => {
   transporter.sendMail(
     {
-      from: `${req.body.full_name}<${req.body.email}>`,
+      from: 'devgybo@gmail.com',
       to: process.env.GYBO_CONTACT,
       subject: req.body.subject || 'Nouveau message de Gybo.ch',
       html: formatTemplate(req.body),
