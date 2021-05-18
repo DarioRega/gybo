a40102763f167dc700516d56354413c8d9d93cd7
dario <dario.regazzoni@outlook.fr>
2021-05-18 20:39:07 +0200

    Fix: supportSrclUrl

--- components/Testimonial.vue
+++ components/Testimonial.vue
@@ -1,30 +1,22 @@
 <template>
-  <section class="testimonial bg-primary overflow-hidden">
-    <div v-if="blok" class="relative mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="relative max-w-5xl mx-auto">
+  <section class="overflow-hidden testimonial bg-primary">
+    <div v-if="blok" class="relative px-4 mx-auto sm:px-6 lg:px-8">
+      <div class="relative mx-auto max-w-5xl">
         <div class="w-full testimonial__container">
           <div
             class="mx-auto testimonial__logo"
             :class="blok.isLandscape ? 'is-landscape' : 'is-portrait'"
           >
             <img
-              v-if="currentTheme === 'dark'"
-              :src="blok.logo.filename"
-              :onerror="blok.logoSupportOldNavs.filename"
-              :alt="blok.logoSupportOldNavs.alt"
-              class="w-full h-full"
-            />
-            <img
-              v-else
-              :src="blok.logoDark.filename"
-              :onerror="blok.logoSupportOldNavsDark.filename"
+              :src="srcUrl"
+              :onerror="supportSrcUrl"
               :alt="blok.logoSupportOldNavs.alt"
               class="w-full h-full"
             />
           </div>
         </div>
         <blockquote class="mt-8">
-          <div class="mx-auto text-center font-medium text-primary">
+          <div class="mx-auto font-medium text-center text-primary">
             <p>{{ blok.text }}</p>
           </div>
           <footer class="mt-8">
@@ -32,12 +24,12 @@
               <div
                 class="mt-3 text-center md:mt-0 md:ml-4 md:flex md:items-center"
               >
-                <div class="text-primary font-medium">
+                <div class="font-medium text-primary">
                   {{ blok.personName }}
                 </div>
 
                 <svg
-                  class="hidden md:block mx-1 testimonial__separator text-tertiary"
+                  class="hidden mx-1 md:block testimonial__separator text-tertiary"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                 >
@@ -69,16 +61,19 @@ export default {
   },
   computed: {
     currentTheme() {
+      console.log('CURRENT THEME', store.currentTheme)
       return store.currentTheme
     },
-    logoUrl() {
-      return this.currentTheme === 'dark'
+    srcUrl() {
+      return this.currentTheme === 'theme-dusk'
         ? this.blok.logo.filename
         : this.blok.logoDark.filename
     },
-  },
-  mounted() {
-    console.log('TJIS BLOK TESTI', this.blok)
+    supportSrcUrl() {
+      return this.currentTheme === 'theme-dusk'
+        ? this.blok.logoSupportOldNavs.filename
+        : this.blok.logoSupportOldNavsDark.filename
+    },
   },
 }
 </script>
@@ -92,6 +87,7 @@ export default {
         width: 50%;
       }
     }
+
     &.is-portrait {
       @apply w-40;
     }
